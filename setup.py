a9dfac1f1c08851b0db76b6fefb0d2875be3e50f
Elias Hachichou <elias.hachichou@adwisit.se>
2019-01-28 14:19:01 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robotframework-wadlibrary",
+    version="0.0.1",
+    author="Elias Hachichou, Adwisit",
+    author_email="elias.hachichou@adwisit.se",
+    description="WADLibrary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Adwisit/WADLibrary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
