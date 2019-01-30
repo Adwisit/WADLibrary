84e320b0f35bfd4c1fa8646eb095af46734d59cd
Elias Hachichou <elias.hachichou@adwisit.se>
2019-01-30 13:25:19 +0100

    Fixed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robotframework-wadlibrary",
-    version="0.0.1",
+    version="0.0.2",
     author="Elias Hachichou, Adwisit",
     author_email="elias.hachichou@adwisit.se",
     description="WADLibrary",
