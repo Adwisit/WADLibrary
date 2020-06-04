bbd537cebff28c95bd0dbc6bacdfdb53ce18c065
Elias Hachichou <elias.hachichou@adwisit.se>
2020-06-04 15:33:48 +0200

    Moved requirements to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Adwisit/WADLibrary",
+    install_requires = ['robotframework', 'requests', 'psutil'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
